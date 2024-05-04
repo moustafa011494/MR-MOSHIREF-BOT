8839c56895d0625e78c849aa9179459a2348ba83
moustafa011494 <127332000+moustafa011494@users.noreply.github.com>
2024-05-04 17:03:12 +0300

    Update rg-register.js

--- plugins/rg-register.js
+++ plugins/rg-register.js
@@ -12,7 +12,7 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
   if (name.length >= 30) throw '✳️ El nombre es demasiado largo' 
   age = parseInt(age)
   if (age > 100) throw '👴🏻 Wow el abuelo quiere jugar al bot'
-  if (age < 5) throw '🚼  hay un abuelo bebé jsjsj '
+  if (age < 10) throw '🚼  hay un abuelo bebé jsjsj '
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
